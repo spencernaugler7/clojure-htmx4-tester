030893b4655d7a1f60cb9462a1d85fd7df0acf07
Spencer Naugler <spencernaugler7@gmail.com>
2026-04-30 00:03:32 -0400

    setup besic routes.

--- src/test_clojure/core.clj
+++ src/test_clojure/core.clj
@@ -1,8 +1,19 @@
 (ns test-clojure.core
+  (:require [compojure.core :refer [defroutes GET POST]]
+            [compojure.route :as route]
+            [ring.util.response :as response]
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]])
   (:gen-class))
 
 
-(defn -main 
-  [& args]
-  (println "Hello")
-  (println args))
+(defroutes app-routes
+  (GET "/" [] (response/redirect "/contacts"))
+  (GET "/contacts" [] "Hello Contacts")
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults #'app-routes site-defaults))
+
+(defn -main []
+  (jetty/run-jetty #'app {:port 3000}))
