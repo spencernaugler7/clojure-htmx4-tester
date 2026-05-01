8d5653152471225988cdb66acc4479565516a112
Spencer Naugler <spencernaugler7@gmail.com>
2026-04-30 20:42:57 -0400

    add simple search response

--- src/test_clojure/core.clj
+++ src/test_clojure/core.clj
@@ -1,15 +1,26 @@
 (ns test-clojure.core
   (:require [compojure.core :refer [defroutes GET POST]]
             [compojure.route :as route]
+            [ring.util.request :as request]
+            [clojure.string :as s]
             [ring.util.response :as response]
             [ring.adapter.jetty :as jetty]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]])
   (:gen-class))
 
+(def contacts #{"jeff" "blan" "john" "doe"})
+
+(defn render-contacts [contacts]
+  )
+
+(defn get-contacts [search]
+  (->> contacts
+       (filter (fn [s] (s/includes? s "j")))
+       (s/join ", ")))
 
 (defroutes app-routes
   (GET "/" [] (response/redirect "/contacts"))
-  (GET "/contacts" [] "Hello Contacts")
+  (GET "/contacts" [] (get-contacts "s"))
   (route/not-found "Not Found"))
 
 (def app
